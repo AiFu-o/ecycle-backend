b0abee62d7ba94acfa8cc0ac2898f66491ac77b1
王炜晨 <>
2024-01-19 15:16:53 +0800

    init

---
+++ ecycle-gateway/src/main/java/com/ecycle/gateway/GateWayServiceApplication.java
@@ -0,0 +1,15 @@
+package com.ecycle.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author wangweichen
+ */
+@SpringBootApplication
+public class GateWayServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AuthServiceApplication.class, args);
+    }
+
+}
